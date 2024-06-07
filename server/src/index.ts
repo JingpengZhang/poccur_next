4f759c6b5efe91c0a324066d8b00da92df7dcdaa
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-06-07 23:30:06 +0800

    📝  feat: 配置 swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import Elysia from "elysia";
 import { users } from "./module/users";
+import swagger from "@elysiajs/swagger";
 
 const app = new Elysia()
+  .use(swagger())
   .get("/", () => "hello poccur_next")
   .use(users)
   .listen(3001);
