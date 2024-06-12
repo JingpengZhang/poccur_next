a044af51c8e05ed6a225c77574f2cb25248cabdb
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-06-12 23:34:55 +0800

    ✨  feat: 增加 category name 字段长度

--- server/src/db/schema/categories.ts
+++ server/src/db/schema/categories.ts
@@ -2,7 +2,7 @@ import { mysqlTable, serial, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 export const categories = mysqlTable("categories", {
   id: serial("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 6 }).unique(),
+  name: varchar("name", { length: 50 }).unique(),
   createAt: timestamp("create_at"),
   updateAt: timestamp("update_at"),
 });
