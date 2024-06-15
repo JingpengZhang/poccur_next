0db6704cf3c9e0ecb5894348e247195ef62e43a5
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-06-15 15:09:55 +0800

    ✨  feat: god layout

---
+++ app/(admin)/god/(main)/layout.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content, Header } from "antd/es/layout/layout";
+import React from "react";
+
+export default function PageLayout({ children }: React.PropsWithChildren) {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <>
+      <Layout className="h-screen">
+        <Header style={{ display: "flex", alignItems: "center" }}>
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            items={[]}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Layout>
+          <Sider width={200} style={{ background: colorBgContainer }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%", borderRight: 0 }}
+            />
+          </Sider>
+          <Layout style={{ padding: "0 24px 24px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              {children}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </>
+  );
+}
