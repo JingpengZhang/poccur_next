4f17b3e125b5403b9979263ad3113fed3b149d6f
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-06-16 09:50:43 +0800

    ✨  feat: config antd local to cn

--- components/AntdWrapper.tsx
+++ components/AntdWrapper.tsx
@@ -2,11 +2,12 @@
 
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ConfigProvider, theme } from "antd";
+import zhCN from "antd/locale/zh_CN";
 
 const AntdWrapper = ({ children }: React.PropsWithChildren) => {
   return (
     <AntdRegistry>
-      <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
+      <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }} locale={zhCN}>
         {children}
       </ConfigProvider>
     </AntdRegistry>
